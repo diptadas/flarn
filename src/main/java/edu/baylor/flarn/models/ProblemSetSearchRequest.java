f53a5b9a2eb2a1d6eb611a5c6f47dca71906a663
diptadas <dipta670@gmail.com>
2019-10-13 01:16:48 -0500

    Add search problem set

---
+++ src/main/java/edu/baylor/flarn/models/ProblemSetSearchRequest.java
@@ -0,0 +1,16 @@
+package edu.baylor.flarn.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ProblemSetSearchRequest {
+    private String title; // substring matching
+
+    private String category;
+
+    private String difficulty;
+}
