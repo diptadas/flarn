b550cb7bc1694b331dc5d48ffbc91839a21081b5
diptadas <dipta670@gmail.com>
2019-10-13 01:16:34 -0500

    Add category and problem set repositories

---
+++ src/main/java/edu/baylor/flarn/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package edu.baylor.flarn.repositories;
+
+import edu.baylor.flarn.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    public Category findByName(String name);
+}
--- src/main/java/edu/baylor/flarn/repositories/ProblemSetRepository.java
+++ src/main/java/edu/baylor/flarn/repositories/ProblemSetRepository.java
@@ -1,7 +1,20 @@
 package edu.baylor.flarn.repositories;
 
+import edu.baylor.flarn.models.Category;
+import edu.baylor.flarn.models.Difficulty;
 import edu.baylor.flarn.models.ProblemSet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
 public interface ProblemSetRepository extends JpaRepository<ProblemSet, Long> {
+
+    public List<ProblemSet> findByDifficultyAndCategoryAndTitleLike(@NotNull Difficulty difficulty, Category category, @NotNull String title);
+
+    public List<ProblemSet> findByDifficultyAndTitleLike(@NotNull Difficulty difficulty, @NotNull String title);
+
+    public List<ProblemSet> findByCategoryAndTitleLike(Category category, @NotNull String title);
+
+    public List<ProblemSet> findByTitleLike(@NotNull String title);
 }
