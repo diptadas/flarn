2cf9534ddb25268bf6cb2bdf7ce1b0a20330c094
Dipta Das <dipta670@gmail.com>
2019-10-14 23:55:16 -0500

    Cleanup data initializer into seperate methods

--- src/main/java/edu/baylor/flarn/resources/DataInitializer.java
+++ src/main/java/edu/baylor/flarn/resources/DataInitializer.java
@@ -21,22 +21,94 @@ public class DataInitializer implements CommandLineRunner {
         this.entityManager = entityManager;
     }
 
+    private List<Category> categories = new ArrayList<>();
+    private List<ProblemSet> problemSets = new ArrayList<>();
+    private List<User> moderators = new ArrayList<>();
+    private List<User> learners = new ArrayList<>();
+    private User admin;
+
     @Override
     @Transactional
     public void run(String... args) throws Exception {
         log.info("Initializing data");
 
-        // creating some category
-        List<Category> categories = new ArrayList<>();
+        createAdmin();
+        createModerators();
+        createLearners();
+        createSubscriptions();
+
+        createCategories();
+        createProblemSets();
+    }
+
+    private void createCategories() {
         for (int i = 0; i < 3; i++) {
             Category category = new Category();
             category.setName("category-" + i);
             entityManager.persist(category);
+            entityManager.flush();
 
             categories.add(category);
         }
+    }
 
-        // adding problem sets
+    private void createModerators() {
+        for (int i = 0; i < 3; i++) {
+            User moderator = new User("moderator" + i + "@gm.com",
+                    "moderator" + i, "Moderator" + i, "254567908", "part",
+                    "temple", "AZ", "0000", new ArrayList<>());
+            moderator.setUserType(UserType.MODERATOR);
+            entityManager.persist(moderator);
+            entityManager.flush();
+
+            moderators.add(moderator);
+        }
+    }
+
+    private void createLearners() {
+        for (int i = 0; i < 3; i++) {
+            User learner = new User("learner" + i + "@gm.com",
+                    "learner" + i, "Learner" + i, "254567908", "part",
+                    "temple", "AZ", "0000", new ArrayList<>());
+            learner.setUserType(UserType.LEARNER);
+            entityManager.persist(learner);
+            entityManager.flush();
+
+            learners.add(learner);
+        }
+    }
+
+    private void createAdmin() {
+        admin = new User("admin@gm.com",
+                "admin", "Admin Mock", "254567908", "part",
+                "temple", "AZ", "0000", new ArrayList<>());
+        admin.setUserType(UserType.ADMIN);
+        entityManager.persist(admin);
+        entityManager.flush();
+    }
+
+    private void createSubscriptions() {
+        moderators.get(1).subscribe(moderators.get(0));
+        entityManager.persist(moderators.get(1)); // don't need to persist both
+        entityManager.flush();
+
+        learners.get(0).subscribe(moderators.get(1));
+        learners.get(0).subscribe(admin);
+        entityManager.persist(learners.get(0)); // don't need to persist both
+        entityManager.flush();
+
+        learners.get(1).subscribe(learners.get(0));
+        learners.get(1).subscribe(moderators.get(1));
+        entityManager.persist(learners.get(1)); // don't need to persist both
+        entityManager.flush();
+
+        learners.get(2).subscribe(learners.get(0));
+        learners.get(2).subscribe(learners.get(1));
+        entityManager.persist(learners.get(1)); // don't need to persist both
+        entityManager.flush();
+    }
+
+    private void createProblemSets() {
         for (int i = 0; i < 10; i++) {
             ProblemSet problemSet = new ProblemSet();
             problemSet.setDifficulty(Difficulty.EASY);
@@ -47,6 +119,7 @@ public class DataInitializer implements CommandLineRunner {
             KnowledgeSource knowledgeSource = new KnowledgeSource();
             knowledgeSource.setContentLink("http://google.com");
             entityManager.persist(knowledgeSource);
+            entityManager.flush();
 
             problemSet.setKnowledgeSource(knowledgeSource);
             problemSet.setTitle("ProblemSet-" + i);
@@ -64,60 +137,15 @@ public class DataInitializer implements CommandLineRunner {
                 }
 
                 entityManager.persist(question);
+                entityManager.flush();
 
-                problemSet.getQuestion().add(question);
+                problemSet.getQuestions().add(question);
             }
 
             entityManager.persist(problemSet);
+            entityManager.flush();
+
+            problemSets.add(problemSet);
         }
-
-        //List<String> roles = new ArrayList<>();
-        User admin = new User("admin@gm.com",
-                "admin", "Admin Mock", "254567908", "part",
-                "temple", "AZ", "0000", new ArrayList<String>());
-        admin.setUserType(UserType.ADMIN);
-        entityManager.persist(admin);
-
-        User moderator1 = new User("mod1@gm.com",
-                "mod1", "Moderator One", "254567908", "part",
-                "temple", "AZ", "0000", new ArrayList<String>());
-        moderator1.setUserType(UserType.MODERATOR);
-        entityManager.persist(moderator1);
-
-
-        User moderator2 = new User("mod2@gm.com",
-                "mod2", "Moderator two", "254567908", "part",
-                "temple", "AZ", "0000", new ArrayList<String>());
-        moderator2.setUserType(UserType.MODERATOR);
-        moderator2.getSubscriptions().add(moderator1);
-        entityManager.persist(moderator2);
-
-
-        User learner1 = new User("learner1@gm.com",
-                "learn1", "Learner One", "254567908", "part",
-                "temple", "AZ", "0000", new ArrayList<String>());
-        learner1.setUserType(UserType.LEARNER);
-        learner1.getSubscriptions().add(admin);
-        learner1.getSubscriptions().add(moderator2);
-        entityManager.persist(learner1);
-
-        User learner2 = new User("learner2@gm.com",
-                "learn2", "Learner two", "254567908", "part",
-                "temple", "AZ", "0000", new ArrayList<String>());
-        learner2.setUserType(UserType.LEARNER);
-        learner2.getSubscriptions().add(admin);
-        learner2.getSubscriptions().add(learner1);
-        entityManager.persist(learner2);
-
-
-        User learner3 = new User("learner3@gm.com",
-                "learn3", "Learner One", "254567908", "part",
-                "temple", "AZ", "0000", new ArrayList<String>());
-        learner3.setUserType(UserType.LEARNER);
-        learner3.getSubscriptions().add(learner2);
-        learner3.getSubscriptions().add(admin);
-        learner3.getSubscriptions().add(moderator1);
-        learner3.getSubscriptions().add(learner1);
-        entityManager.persist(learner3);
     }
 }
