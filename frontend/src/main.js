ba994b151a9e946411070e59ff488390d18e312d
Clinton Yeboah <clintonyeb@gmail.com>
2019-11-13 10:50:10 -0600

    Clean up and setting up project defaults

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,16 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+// Boostrap JS Files
+import jQuery from "jquery";
+window.jQuery = window.$ = jQuery
+import Popper from 'popper.js'
+window.Popper = Popper
+import 'bootstrap'
+import Headroom from "headroom.js";
+window.Headroom = Headroom;
+require('./assets/js/argon.js')
+
 new Vue({
   router,
   store,
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,17 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    globalButton: "login"
   },
   mutations: {
+    SET_GLOBAL_BUTTON(state, newState) {
+      state.globalButton = newState;
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {}
+});
