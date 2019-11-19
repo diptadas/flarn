54130c532929fb7ed41a35821a788f52fdc3ee56
Clinton Yeboah <clintonyeb@gmail.com>
2019-11-19 03:16:54 -0600

    Adds People pages: People List, Person Detail View

---
+++ frontend/src/components/people/Person.vue
@@ -0,0 +1,117 @@
+<template>
+  <tr class="pointed hover-grey" @click="goToProfile">
+    <th scope="row">
+      <div class="media align-items-center">
+        <a href="#" class="avatar rounded-circle mr-3">
+          <img
+            alt="Image placeholder"
+            src="../../assets/img/theme/team-1-800x800.jpg"
+          />
+        </a>
+        <div class="media-body">
+          <span class="mb-0 text-sm">{{ user.fullName }}</span>
+        </div>
+      </div>
+    </th>
+    <td>
+      <span class="badge badge-dot d-inline-block pt-3">
+        <i class="bg-warning"></i> {{ user.userType }}
+      </span>
+    </td>
+    <td>
+      <div class="avatar-group">
+        <a
+          href="#"
+          class="avatar avatar-sm"
+          data-toggle="tooltip"
+          data-original-title="Ryan Tompson"
+        >
+          <img
+            alt="Image placeholder"
+            src="../../assets/img/theme/team-1-800x800.jpg"
+            class="rounded-circle"
+          />
+        </a>
+        <a
+          href="#"
+          class="avatar avatar-sm"
+          data-toggle="tooltip"
+          data-original-title="Romina Hadid"
+        >
+          <img
+            alt="Image placeholder"
+            src="../../assets/img/theme/team-2-800x800.jpg"
+            class="rounded-circle"
+          />
+        </a>
+        <a
+          href="#"
+          class="avatar avatar-sm"
+          data-toggle="tooltip"
+          data-original-title="Alexander Smith"
+        >
+          <img
+            alt="Image placeholder"
+            src="../../assets/img/theme/team-3-800x800.jpg"
+            class="rounded-circle"
+          />
+        </a>
+        <a
+          href="#"
+          class="avatar avatar-sm"
+          data-toggle="tooltip"
+          data-original-title="Jessica Doe"
+        >
+          <img
+            alt="Image placeholder"
+            src="../../assets/img/theme/team-4-800x800.jpg"
+            class="rounded-circle"
+          />
+        </a>
+      </div>
+    </td>
+    <td>
+      <div class="d-flex align-items-center">
+        <span class="">700 points</span>
+      </div>
+    </td>
+    <!-- <td class="text-right">
+      <div class="dropdown">
+        <a
+          class="btn btn-sm btn-icon-only text-light"
+          href="#"
+          role="button"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          <i class="fas fa-ellipsis-v"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </div>
+    </td> -->
+  </tr>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  },
+  name: "Person",
+  methods: {
+    goToProfile() {
+      this.$router.push({ name: "user-profile", params: { id: this.user.id } });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- frontend/src/views/home/People.vue
+++ frontend/src/views/home/People.vue
@@ -1,13 +1,165 @@
 <template>
-  <div>
-    People
+  <div class="column">
+    <div class="mt-2">
+      <ul
+        class="nav nav-pills nav-fill flex-column flex-sm-row"
+        id="tabs-text"
+        role="tablist"
+      >
+        <li class="nav-item" v-for="tab in tabs" :key="tab.id">
+          <a
+            class="nav-link mb-sm-3 mb-md-0"
+            id="tabs-text-1-tab"
+            data-toggle="tab"
+            href="#tabs-text-1"
+            role="tab"
+            aria-controls="tabs-text-1"
+            aria-selected="true"
+            :class="activeTab === tab.id ? ' active' : ''"
+            @click="changeTabView(tab.id)"
+            >{{ tab.text }}</a
+          >
+        </li>
+      </ul>
+    </div>
+
+    <div class="row mt-4">
+      <div class="col">
+        <div class="card shadow">
+          <div class="table-responsive">
+            <table class="table align-items-center table-flush">
+              <tbody>
+                <person-item
+                  v-for="user in users"
+                  :key="user.id"
+                  :user="user"
+                  class="my-4"
+                ></person-item>
+              </tbody>
+            </table>
+          </div>
+          <div class="card-footer py-4">
+            <nav aria-label="...">
+              <ul class="pagination justify-content-end mb-0">
+                <li class="page-item disabled">
+                  <a class="page-link" href="#" tabindex="-1">
+                    <i class="fa fa-arrow-left"></i>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                </li>
+                <li class="page-item active">
+                  <a class="page-link" href="#">1</a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link" href="#">
+                    2
+                    <span class="sr-only">(current)</span>
+                  </a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                  <a class="page-link" href="#">
+                    <i class="fa fa-arrow-right"></i>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import PersonItem from "@/components/people/Person.vue";
+
 export default {
-  name: "People"
+  name: "People",
+  data() {
+    return {
+      activeTab: 1,
+      users: [],
+      tabs: [
+        {
+          id: 1,
+          text: "Followers",
+          value: "subscribers"
+        },
+        {
+          id: 2,
+          text: "Following",
+          value: "subscriptions"
+        },
+        {
+          id: 3,
+          text: "All Users",
+          value: "all"
+        }
+      ]
+    };
+  },
+  methods: {
+    changeTabView(tabId) {
+      if (this.activeTab === tabId) return;
+
+      switch (tabId) {
+        case 1:
+          this.getSubscribedUsers();
+          break;
+        case 2:
+          this.getSubscriptions();
+          break;
+        case 3:
+          this.getUsers();
+          break;
+      }
+
+      this.activeTab = tabId;
+    },
+    getSubscriptions() {
+      const url = `users/${this.$store.state.userId}/subscriptions`;
+
+      this.$http
+        .get(url)
+        .then(res => {
+          this.users = res.data;
+        })
+        .catch(err => {});
+    },
+    getSubscribedUsers() {
+      const url = `users/${this.$store.state.userId}/subscribers`;
+
+      this.$http
+        .get(url)
+        .then(res => {
+          this.users = res.data;
+        })
+        .catch(err => {});
+    },
+    getUsers() {
+      const url = "users";
+
+      this.$http
+        .get(url)
+        .then(res => {
+          this.users = res.data;
+        })
+        .catch(err => {});
+    }
+  },
+  created() {
+    this.getSubscribedUsers();
+  },
+  components: {
+    PersonItem
+  }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+table {
+  border-spacing: 2em;
+}
+</style>
