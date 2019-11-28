983782e07dbaf33e44b6be7b6e6307a4a842bc8f
Dipta Das <dipta670@gmail.com>
2019-11-28 03:21:01 -0600

    Auto generate empty test templates for services

---
+++ backend/src/test/java/edu/baylor/flarn/services/EmailServiceTest.java
@@ -0,0 +1,24 @@
+package edu.baylor.flarn.services;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmailServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void sendEmail() {
+    }
+
+    @Test
+    public void sendVerificationEmail() {
+    }
+}
