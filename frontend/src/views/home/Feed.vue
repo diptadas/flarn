39bc1039f5a9e12119f0946d4ec5aea83fb791ae
Clinton Yeboah <clintonyeb@gmail.com>
2019-11-28 10:23:21 -0600

    Added AttemptedProblem, Joined, UpdatedProfile. Created Enum for activity types

--- frontend/src/views/home/Feed.vue
+++ frontend/src/views/home/Feed.vue
@@ -3,24 +3,39 @@
     <template v-for="act in activities">
       <Followed
         :key="act.id"
-        v-if="act.activityType === 'FOLLOWED'"
+        v-if="act.activityType === activityType.FOLLOWED"
         :activity="act"
       ></Followed>
       <Stared
         :key="act.id"
-        v-else-if="act.activityType === 'STARED'"
+        v-else-if="act.activityType === activityType.STARED"
         :activity="act"
       ></Stared>
       <Commented
         :key="act.id"
-        v-else-if="act.activityType === 'COMMENTED'"
+        v-else-if="act.activityType === activityType.COMMENTED"
         :activity="act"
       ></Commented>
       <CreatedProblem
         :key="act.id"
-        v-else-if="act.activityType === 'CREATED_PROBLEM'"
+        v-else-if="act.activityType === activityType.CREATED_PROBLEM"
         :activity="act"
       ></CreatedProblem>
+      <AttemptedProblem
+        :key="act.id"
+        v-else-if="act.activityType === activityType.ATTEMPTED_PROBLEM"
+        :activity="act"
+      ></AttemptedProblem>
+      <Joined
+        :key="act.id"
+        v-else-if="act.activityType === activityType.JOINED"
+        :activity="act"
+      ></Joined>
+      <UpdatedProfile
+        :key="act.id"
+        v-else-if="act.activityType === activityType.UPDATED_PROFILE"
+        :activity="act"
+      ></UpdatedProfile>
     </template>
   </div>
 </template>
@@ -30,6 +45,19 @@ import Followed from "@/components/activities/Followed";
 import Stared from "@/components/activities/Stared";
 import Commented from "@/components/activities/Commented";
 import CreatedProblem from "@/components/activities/CreatedProblem";
+import AttemptedProblem from "@/components/activities/AttemptedProblem";
+import Joined from "@/components/activities/Joined";
+import UpdatedProfile from "@/components/activities/UpdatedProfile";
+
+const ActivityType = Object.freeze({
+  JOINED: "JOINED",
+  UPDATED_PROFILE: "UPDATED_PROFILE",
+  FOLLOWED: "FOLLOWED",
+  ATTEMPTED_PROBLEM: "ATTEMPTED_PROBLEM",
+  CREATED_PROBLEM: "CREATED_PROBLEM",
+  STARED: "STARED",
+  COMMENTED: "COMMENTED"
+});
 
 export default {
   name: "Feed",
@@ -38,11 +66,19 @@ export default {
       type: Array
     }
   },
+  data() {
+    return {
+      activityType: ActivityType
+    };
+  },
   components: {
     Followed,
     Stared,
     Commented,
-    CreatedProblem
+    CreatedProblem,
+    AttemptedProblem,
+    Joined,
+    UpdatedProfile
   }
 };
 </script>
