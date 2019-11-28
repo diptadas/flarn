cf681b9332ee6c986600b6406d762b6a8eb5a3cf
Clinton Yeboah <clintonyeb@gmail.com>
2019-11-28 01:23:37 -0600

    Created UI for stories

---
+++ frontend/src/views/home/Stories.vue
@@ -0,0 +1,30 @@
+<template>
+  <Feed :activities="activities" />
+</template>
+
+<script>
+import Feed from "@/views/home/Feed.vue";
+export default {
+  name: "Timeline",
+  data() {
+    return {
+      activities: []
+    };
+  },
+  methods: {
+    getActivities() {
+      const url = "/users/current/subscriptionsActivities";
+
+      this.$http.get(url).then(res => {
+        this.activities = res.data;
+      });
+    }
+  },
+  created() {
+    this.getActivities();
+  },
+  components: {
+    Feed
+  }
+};
+</script>
