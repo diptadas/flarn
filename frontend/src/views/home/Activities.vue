542fe3d9082eb87337695827df3a0363a0c52705
Clinton Yeboah <clintonyeb@gmail.com>
2019-11-28 01:23:53 -0600

    Created Activities UI

---
+++ frontend/src/views/home/Activities.vue
@@ -0,0 +1,31 @@
+<template>
+  <Feed :activities="activities" />
+</template>
+
+<script>
+import Feed from "@/views/home/Feed.vue";
+
+export default {
+  name: "Activities",
+  data() {
+    return {
+      activities: []
+    };
+  },
+  methods: {
+    getActivities() {
+      const url = "users/current/activities";
+
+      this.$http.get(url).then(res => {
+        this.activities = res.data;
+      });
+    }
+  },
+  created() {
+    this.getActivities();
+  },
+  components: {
+    Feed
+  }
+};
+</script>
