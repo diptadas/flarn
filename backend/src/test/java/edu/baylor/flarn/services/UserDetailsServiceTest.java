d4ac95724a23ebb71801a86520e461e9c66c6fcb
boadu <boadu203@gmail.com>
2019-11-29 14:24:13 -0600

    Adding other unit test

---
+++ backend/src/test/java/edu/baylor/flarn/services/UserDetailsServiceTest.java
@@ -0,0 +1,12 @@
+package edu.baylor.flarn.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDetailsServiceTest {
+
+    @Test
+    void loadUserByUsername() {
+    }
+}
