a97706cbb65d33a5e681c41f627bc4365c8d7999
Dipta Das <dipta670@gmail.com>
2019-11-29 16:12:49 -0600

    Move to exceptions package

--- backend/src/main/java/edu/baylor/flarn/exceptions/InvalidJwtAuthenticationException.java
+++ backend/src/main/java/edu/baylor/flarn/exceptions/InvalidJwtAuthenticationException.java
@@ -1,11 +1,9 @@
-package edu.baylor.flarn.security;
+package edu.baylor.flarn.exceptions;
 
 import org.springframework.security.core.AuthenticationException;
 
 public class InvalidJwtAuthenticationException extends AuthenticationException {
-    /**
-     *
-     */
+
     private static final long serialVersionUID = -761503632186596342L;
 
     public InvalidJwtAuthenticationException(String e) {
