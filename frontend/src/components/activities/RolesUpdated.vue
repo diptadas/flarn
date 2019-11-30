1c4dcaf944385fd00360fefa298281cf13eaa7c9
Clinton Yeboah <clintonyeb@gmail.com>
2019-11-30 07:06:24 -0600

    Adds role updated to feeds

---
+++ frontend/src/components/activities/RolesUpdated.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="event">
+        <div class="label">
+            <img :src="activity.userAvatarLink" />
+        </div>
+        <div class="content">
+            <div class="summary">
+                <router-link
+                        :to="{
+            name: 'user-profile',
+            params: { id: $hash.encode(activity.userId) }
+          }"
+                >{{ activity.userFullName }}</router-link
+                >
+                is now a {{activity.userType}}
+                <div class="date">
+                    {{ activity.date | formatDate }}
+                </div>
+            </div>
+            <div class="meta">
+                <a class="like"> <i class="like icon"></i> 5 Likes </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import formateDate from "./formateDate";
+
+    export default {
+        props: {
+            activity: {
+                type: Object
+            }
+        },
+        name: "RolesUpdated",
+        mixins: [formateDate]
+    }
+</script>
--- frontend/src/views/home/Feed.vue
+++ frontend/src/views/home/Feed.vue
@@ -1,42 +1,9 @@
 <template>
   <div class="ui feed">
-    <template v-for="act in activities">
-      <Followed
-        :key="act.id"
-        v-if="act.activityType === activityType.FOLLOWED"
-        :activity="act"
-      ></Followed>
-      <Stared
-        :key="act.id"
-        v-else-if="act.activityType === activityType.STARED"
-        :activity="act"
-      ></Stared>
-      <Commented
-        :key="act.id"
-        v-else-if="act.activityType === activityType.COMMENTED"
-        :activity="act"
-      ></Commented>
-      <CreatedProblem
-        :key="act.id"
-        v-else-if="act.activityType === activityType.CREATED_PROBLEM"
-        :activity="act"
-      ></CreatedProblem>
-      <AttemptedProblem
-        :key="act.id"
-        v-else-if="act.activityType === activityType.ATTEMPTED_PROBLEM"
-        :activity="act"
-      ></AttemptedProblem>
-      <Joined
-        :key="act.id"
-        v-else-if="act.activityType === activityType.JOINED"
-        :activity="act"
-      ></Joined>
-      <UpdatedProfile
-        :key="act.id"
-        v-else-if="act.activityType === activityType.UPDATED_PROFILE"
-        :activity="act"
-      ></UpdatedProfile>
-    </template>
+    <component v-for="act in activities"
+               v-bind:is="act.activityType"
+               :key="act.id"
+               :activity="act" />
   </div>
 </template>
 
@@ -48,16 +15,7 @@ import CreatedProblem from "@/components/activities/CreatedProblem";
 import AttemptedProblem from "@/components/activities/AttemptedProblem";
 import Joined from "@/components/activities/Joined";
 import UpdatedProfile from "@/components/activities/UpdatedProfile";
-
-const ActivityType = Object.freeze({
-  JOINED: "JOINED",
-  UPDATED_PROFILE: "UPDATED_PROFILE",
-  FOLLOWED: "FOLLOWED",
-  ATTEMPTED_PROBLEM: "ATTEMPTED_PROBLEM",
-  CREATED_PROBLEM: "CREATED_PROBLEM",
-  STARED: "STARED",
-  COMMENTED: "COMMENTED"
-});
+import RolesUpdated from "../../components/activities/RolesUpdated";
 
 export default {
   name: "Feed",
@@ -66,19 +24,15 @@ export default {
       type: Array
     }
   },
-  data() {
-    return {
-      activityType: ActivityType
-    };
-  },
   components: {
-    Followed,
-    Stared,
-    Commented,
-    CreatedProblem,
-    AttemptedProblem,
-    Joined,
-    UpdatedProfile
+    FOLLOWED: Followed,
+    STARED: Stared,
+    COMMENTED: Commented,
+    CREATED_PROBLEM: CreatedProblem,
+    ATTEMPTED_PROBLEM: AttemptedProblem,
+    JOINED: Joined,
+    UPDATED_PROFILE: UpdatedProfile,
+    ROLES_UPDATED: RolesUpdated
   }
 };
 </script>
