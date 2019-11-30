8182926e743aba19d2d133bb6e3b3b1c4bd3d999
Dipta Das <dipta670@gmail.com>
2019-11-30 16:42:40 -0600

    Add Transactional annotation for UserServiceTest

--- backend/src/test/java/edu/baylor/flarn/services/UserServiceTest.java
+++ backend/src/test/java/edu/baylor/flarn/services/UserServiceTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -36,6 +37,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 class UserServiceTest {
 
     @Autowired
