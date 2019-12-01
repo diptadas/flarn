6c3a459436e81a7180d9ff8cfed3e0f34381b27b
boadu <boadu203@gmail.com>
2019-12-01 02:44:47 -0600

    --Activity test

--- backend/src/test/java/edu/baylor/flarn/services/ActivityServiceTest.java
+++ backend/src/test/java/edu/baylor/flarn/services/ActivityServiceTest.java
@@ -1,20 +1,48 @@
 package edu.baylor.flarn.services;
 
+
+import edu.baylor.flarn.models.Activity;
+import edu.baylor.flarn.models.User;
+import org.hamcrest.core.Is;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 class ActivityServiceTest {
 
+    @Autowired
+    private ActivityService activityService;
+
+    @Autowired
+    private UserService userService;
+
+
     @Test
     void getActivitiesForUser() {
-
+        List<Activity> userActivities = activityService.getActivitiesForUser(1L);
+        userActivities.forEach(activity -> Assert.assertThat(activity.getUserId(), Is.is(1L)));
     }
 
     @Test
-    void getActivitiesForUserSubscriptions() {
+    void getActivitiesForUserSubscriptions(){
+        Set<User> subscriptions = userService.getUser(1L).getSubscriptions();
+        List<Long> subscriptionIds = new ArrayList<>();
+
+        for(User user: subscriptions){
+            subscriptionIds.add(user.getId());
+        }
+
+        List<Activity> activities = activityService.getActivitiesForUserSubscriptions(subscriptionIds);
+        activities.forEach(activity -> Assert.assertTrue(subscriptionIds.contains(activity.getUserId())));
     }
 
     @Test
     void saveJoinedActivity() {
+
     }
 
     @Test
