28e4034f90afb61d11b996c9d64b878c2f8639f6
boadu <boadu203@gmail.com>
2019-12-01 05:18:39 -0600

    Review test

--- backend/src/test/java/edu/baylor/flarn/services/ReviewServiceTest.java
+++ backend/src/test/java/edu/baylor/flarn/services/ReviewServiceTest.java
@@ -1,15 +1,68 @@
 package edu.baylor.flarn.services;
 
+import edu.baylor.flarn.exceptions.AlreadyStaredException;
+import edu.baylor.flarn.exceptions.RecordNotFoundException;
+import edu.baylor.flarn.models.Problem;
+import edu.baylor.flarn.models.Review;
+import edu.baylor.flarn.models.User;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Contains the unit  & Integration test for ReviewServiceTest
+ *
+ * @author Dipta Das
+ * @author Clinton Yeboah
+ * @author Frimpong Boadu
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
 class ReviewServiceTest {
 
+    @Autowired
+    private ProblemService problemService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ReviewService reviewService;
+
     @Test
-    void starProblem() {
+    void starProblem() throws RecordNotFoundException, AlreadyStaredException {
+        //get problem
+        Problem problem = problemService.getProblemById(1L);
+        User user = userService.findById(1L);
+
+        Review review = reviewService.starProblem(problem.getId(),user);
+
+        assertNotNull(review);
+
+        Problem problem2 = problemService.getProblemById(1L);
+
+        assertTrue(problem2.getReviews().contains(review));
+
+        assertThatThrownBy(() -> reviewService.starProblem(problem.getId(),user)).isInstanceOf(AlreadyStaredException.class).hasMessageContaining("Already stared problem "+problem.getId());
     }
 
     @Test
-    void unstarProblem() {
+    void unstarProblem() throws RecordNotFoundException {
+
+        //get problem
+        Problem problem = problemService.getProblemById(1L);
+        User user = userService.findById(1L);
+
+        reviewService.unstarProblem(problem.getId(),user);
     }
 
     @Test
