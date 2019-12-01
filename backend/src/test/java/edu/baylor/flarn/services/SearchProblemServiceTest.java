7f41a20907db22bafc998ccbda6154dc6fb1624c
boadu <boadu203@gmail.com>
2019-12-01 04:59:27 -0600

    Problem search test

--- backend/src/test/java/edu/baylor/flarn/services/SearchProblemServiceTest.java
+++ backend/src/test/java/edu/baylor/flarn/services/SearchProblemServiceTest.java
@@ -1,10 +1,53 @@
 package edu.baylor.flarn.services;
 
+import edu.baylor.flarn.models.Category;
+import edu.baylor.flarn.models.Difficulty;
+import edu.baylor.flarn.models.Problem;
+import edu.baylor.flarn.resources.ProblemSearchRequest;
+import org.hamcrest.core.Is;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Contains the unit & Integration for SearchProblemService
+ *
+ * @author Dipta Das
+ * @author Clinton Yeboah
+ * @author Frimpong Boadu
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
 class SearchProblemServiceTest {
+
+    @Autowired
+    private SearchProblemService searchProblemService;
 
     @Test
     void searchProblem() {
+
+        //Problem request
+
+        ProblemSearchRequest problemSearchRequest = new ProblemSearchRequest();
+        problemSearchRequest.setCategory(Category.DEFAULT_CATEGORY_NAME);
+        problemSearchRequest.setDifficulty(Difficulty.HARD.name());
+        problemSearchRequest.setTitle("");
+
+        List<Problem> problems = searchProblemService.searchProblem(problemSearchRequest);
+        //using ""; so return not null
+        assertNotNull(problems);
+        problems.forEach(problem -> Assert.assertThat(problem.getCategory(), Is.is(Category.DEFAULT_CATEGORY_NAME)));
+        problems.forEach(problem -> Assert.assertThat(problem.getDifficulty(), Is.is(Difficulty.HARD.name())));
     }
+
+
 }
